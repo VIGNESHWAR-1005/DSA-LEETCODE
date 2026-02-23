df35b04701bac5b99245607081c8a24cb38700d7
VIGNESWAR REDDY MEKALA <139888164+VIGNESHWAR-1005@users.noreply.github.com>
2026-02-23 15:09:28 +0530

    Time: 5 ms (52.79%) | Memory: 23 MB (14.42%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -8,6 +8,15 @@ class NumArray:
 
     def sumRange(self, left: int, right: int) -> int:
         return self.prefix[right + 1] - self.prefix[left]
+        freq = {}
+        result = []
+        for num in nums1:
+            freq[num] = freq.get(num, 0) + 1    
+        for num in nums2:
+            if num in freq and freq[num] > 0:
+                result.append(num)
+                freq[num] -= 1
+        return result
         
 
 
